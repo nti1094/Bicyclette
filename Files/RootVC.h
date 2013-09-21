b5dd1a1f88886c11af99b1c0d4d8664039ba7516
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2013-09-21 22:38:23 +0200

    Remove Help

--- Files/RootVC.h
+++ Files/RootVC.h
@@ -7,20 +7,16 @@
 //
 
 #import "FanContainerViewController.h"
-#import "HelpVC.h"
 
 @class Station;
 @class CitiesController;
 
-@interface RootVC : FanContainerViewController  <HelpVCDelegate>
+@interface RootVC : FanContainerViewController
 
 // Make outlets public, because they are set in MainWindow.nib
-@property IBOutlet HelpVC *helpVC;
 @property IBOutlet UIToolbar *infoToolbar;
 @property IBOutlet UIButton *infoButton;
 
 @property (nonatomic) CitiesController * citiesController;
-
-- (IBAction)showHelp; // Sent to the first responder from a Pref button
 
 @end
