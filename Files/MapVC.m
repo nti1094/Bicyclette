01de27aef05da8996681a3250737bce11b9564f1
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2013-09-21 23:14:34 +0200

    Fix Attribution label relocation for iOS 7

--- Files/MapVC.m
+++ Files/MapVC.m
@@ -155,10 +155,10 @@
     [self forceFrontmost];
 }
 
-- (void) viewWillAppear:(BOOL)animated
+- (void) viewDidLayoutSubviews
 {
-    [super viewWillAppear:animated];
-    [self.mapView relocateAttributionLogoIfNecessary];
+    [super viewDidLayoutSubviews];
+    [self.mapView relocateAttributionLabelIfNecessary];
 }
 
 - (void) viewDidAppear:(BOOL)animated
@@ -236,7 +236,7 @@
 - (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
 {
     [super didRotateFromInterfaceOrientation:fromInterfaceOrientation];
-    [self.mapView relocateAttributionLogoIfNecessary];
+    [self.mapView relocateAttributionLabelIfNecessary];
 }
 
 - (BOOL) shouldAutorotate
